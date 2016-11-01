ff3ea1fd7e938f172115182e55c48aef2912013c
alex-chuyko <alex.chuyko9@gmail.com>
2016-11-01 22:16:05 +0300

    update

---
+++ resources/views/common/header.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+    <link rel="stylesheet" href="/resources/bootstrap/css/bootstrap.min.css">
+    <script src="/resources/bootstrap/js/bootstrap.min.js"></script>
+    <title>{{ $title }}</title>
+</head>
+<body>
+    @yield('content')
+</body>
+</html>
