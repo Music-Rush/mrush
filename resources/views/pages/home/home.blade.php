8ab428284b4b783ef184569f1c3956769ea6c8ab
VladislavYurgel <Worldstes@gmail.com>
2016-11-01 22:31:06 +0300

    updated

--- resources/views/pages/home/home.blade.php
+++ resources/views/pages/home/home.blade.php
@@ -1,16 +1,7 @@
-<<<<<<< HEAD
 @extends('common.header')
 
 @section('content')
     <div class="container">
         Kek
     </div>
-=======
-@extends('common.header')
-
-@section('content')
-    <div class="container">
-        Kek
-    </div>
->>>>>>> 5a2893ab35fae9cf18668250cc770ef8d60fb877
 @stop
